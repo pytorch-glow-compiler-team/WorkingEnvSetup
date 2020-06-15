f2d4d1be22857217e0e291c5a11a3f21da95b22a
John <qzhang03022@gmail.com>
2020-06-14 21:39:34 -0700

    add python script on build_glow

--- tools/build_glow.sh
+++ tools/build_glow.sh
@@ -1,5 +1,13 @@
 set -e
+
+## step 1: build
 mkdir build
 cd build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug ../
 ninja all
+
+# step 2: download all data under build folder if need. Default is off
+#python ../utils/download_datasets_and_models.py -D -C -O
+
+# step 3: run all unit test
+
