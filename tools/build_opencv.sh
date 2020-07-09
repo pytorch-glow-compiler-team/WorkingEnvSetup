770228acfe325edbbe1d0a5600a6456211a59031
John <qzhang03022@gmail.com>
2020-07-09 08:31:47 -0700

    update docker file

--- tools/build_opencv.sh
+++ tools/build_opencv.sh
@@ -8,6 +8,7 @@ cmake -G Ninja \
     -DBUILD_DOCS=OFF \
     -DBUILD_EXAMPLES=OFF \
     -DPYTHON3_EXECUTABLE=/opt/conda/bin/python3 \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
     ../
 ninja all -j6
 make install
