559540e79c76b1881aeba13b7dd3cd2c687f2b1a
John <qzhang03022@gmail.com>
2020-07-09 08:51:37 -0700

    add setup_torch_env.sh

--- tools/setup_torch_env.sh
+++ tools/setup_torch_env.sh
@@ -1,1 +1,7 @@
 ln -s /usr/local/bin/cmake /usr/bin/cmake
+cd /tmp
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-cxx11-abi-shared-with-deps-latest.zip
+unzip libtorch-cxx11-abi-shared-with-deps-latest.zip
+rm -rf libtorch-cxx11-abi-shared-with-deps-latest.zip
+echo "finish downloading libtorch and extraction\n"
+echo "please set Torch_DIR=/tmp/libtorch/share/cmake/Torch/"
